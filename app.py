d24fa68f7d31dd49b024e950147867fa7f684e5e
Jinyoon Kim <jinyoonok@gmail.com>
2023-10-07 15:19:21 -0400

    last

--- app.py
+++ app.py
@@ -120,17 +120,3 @@ def delete_item(item_id):
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-# Main objective: get familiar with developing web-based applications and CRUD operations.
-# Suppose you have a list of items. The items here could be the products of an online store, or the
-# employees that work for an organization. Each item should have at least 4 attributes: ID, name,
-# description, and image.
-# Implement the following operations:
-# • Browse the list of items.
-# • Sort the list by item ID, or by item name.
-# • Search an item by a keyword or by ID.
-# • Add a new item to the list (assuming the ID is auto generated).
-# • Select an item to edit (assuming you can edit any attribute except for the ID).
-# • Remove an item.
-# All operations are web-based.
-# You are free to select the programming language and the database management system.
